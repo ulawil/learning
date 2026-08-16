b39095dc643511bcde0c9fcc1d52c476ae2b798a
ulawil <ulawil97@gmail.com>
2026-08-16 18:48:45 +0200

    add spring boot notes

---
+++ learning-spring-mvc/src/main/java/com/uleczka/LearningSpringMvcApp.java
@@ -0,0 +1,11 @@
+package com.uleczka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringMvcApp {
+    static void main() {
+        SpringApplication.run(LearningSpringBootApp.class);
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
 rootProject.name = "learning"
 include("learning-spring-core")
 include("learning-spring-aop")
+include("learning-spring-boot")
+include("learning-spring-mvc")
